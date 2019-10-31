bd9eef14fc2ac7666e4afcc3eebf53b666834fae
fancy55 <836955157@qq.com>
2019-10-31 20:26:28 +0800

    分页展示ok

--- src/main/java/com/service/leaveService.java
+++ src/main/java/com/service/leaveService.java
@@ -1,6 +1,7 @@
 package com.service;
 
 import com.dto.LeaveDTO;
+import com.dto.PageDTO;
 import com.mapper.LeaveMapper;
 import com.mapper.UserMapper;
 import com.model.Leave;
@@ -34,5 +35,27 @@ public class leaveService {
             leaveDTOS.add(leaveDTO);
         }
         return leaveDTOS;
+    }
+
+    public PageDTO getLeaves(Integer page, Integer size){//page，size设置分页
+        PageDTO pageDTO = new PageDTO();
+        Integer totalCount = leaveMapper.Count();
+        pageDTO.setPageSplit(totalCount,page,size);//所有的数量/size
+        if(page > pageDTO.getPageTotal())page = pageDTO.getPageTotal();
+        else if(page < 1)page = 1;
+        Integer offset = size*(page-1);
+        List<Leave> leaves = leaveMapper.getLeaves(offset, size);
+        List<LeaveDTO> leaveDTOS = new ArrayList<>();
+        for(Leave leave:leaves){
+            User user = userMapper.findById(leave.getCreator());
+            LeaveDTO leaveDTO = new LeaveDTO();
+            //把leave对象换到dto中
+            BeanUtils.copyProperties(leave,leaveDTO);//(1,2)快速把1的所有属性拷贝到2中
+            leaveDTO.setUser(user);
+            //System.out.println(user.toString()); //下划线——驼峰
+            leaveDTOS.add(leaveDTO);
+        }
+        pageDTO.setLeave(leaveDTOS);
+        return pageDTO;
     }
 }
